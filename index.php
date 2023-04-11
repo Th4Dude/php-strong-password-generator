97312dc76d2053920d43ed1726ccc1366a584c27
Th4Dude <gallottimaurizio@yahoo.it>
2023-04-11 18:10:38 +0300

    form

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password Generator</title>
+</head>
+<body>
+
+    <h1>Generatore di password casuale</h1>
+    <form action="index.php" method="get">
+        <label for="password-length">Lunghezza password:</label>
+        <input type="number" id="password-length" name="password-length" min="1" max="100">
+        <input type="submit" value="Genera password">
+    </form>
+
+    
+</body>
+</html>
